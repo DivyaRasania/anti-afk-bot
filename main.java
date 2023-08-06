d132b2f2e30ab9572f6c3d441b03750298f5af2a
Divya Rasania <rasaniadivya1@gmail.com>
2023-08-06 16:44:14 -0400

     

--- main.java
+++ main.java
@@ -1,21 +1,31 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.Random;
-import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws AWTException, InterruptedException {
         Robot rb = new Robot();
 
         while (true) {
-            Thread.sleep(1000);
-            rb.keyPress(KeyEvent.VK_W);
-            rb.keyRelease(KeyEvent.VK_W);
 
-            Thread.sleep(100);
-
-            rb.keyPress(KeyEvent.VK_D);
-            rb.keyRelease(KeyEvent.VK_D);
+            /*
+             * Thread.sleep(1000);
+             * rb.keyPress(KeyEvent.VK_W);
+             * rb.keyRelease(KeyEvent.VK_W);
+             * 
+             * Thread.sleep(100);
+             * 
+             * rb.keyPress(KeyEvent.VK_D);
+             * rb.keyRelease(KeyEvent.VK_D);
+             */
+            
+            /*
+             * Thread.sleep(5000);
+             * 
+             * int x = (int) (Math.random() * 1000);
+             * int y = (int) (Math.random() * 1000);
+             * 
+             * rb.mouseMove(x, y);
+             */
         }
     }
 }
